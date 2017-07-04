8477263287a60431efc79eaffa05046a4bcb49a6
eric mcgregor <eric.mcgregor@gmail.com>
2017-07-04 06:33:11 -0500

    removed all files

--- imports/startup/server.js
+++
@@ -1,7 +0,0 @@
-import {Meteor} from 'meteor/meteor';
-import Data from '/imports/api/Data'
-import '/imports/projects/server'
-
-Meteor.startup(() => {
-
-});
--- imports/ui/components/PageHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PageHeader = (props) => (
-  <header className="PageHeader">
-    {props.children}
-  </header>
-)
-
-export default PageHeader;
